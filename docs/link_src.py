0665e4cd26b9b6a55d43d44b766bb0ac75a250e4
Sebastian_Lehner <a1303583@srvx1.img.univie.ac.at>
2019-05-23 19:52:11 +0200

    auto commit minor changes via bash script

---
+++ docs/link_src.py
@@ -0,0 +1,7 @@
+#sys.path.append('/path/to/application/app/folder')
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+import notebooks.aux.data_download
