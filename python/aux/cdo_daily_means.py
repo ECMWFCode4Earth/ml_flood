4f0bbb3bead880a60e746185b775d5faa860a813
Sebastian_Lehner <a1303583@srvx1.img.univie.ac.at>
2019-05-30 01:51:19 +0200

    auto commit minor changes via bash script

--- python/aux/cdo_daily_means.py
+++ python/aux/cdo_daily_means.py
@@ -3,10 +3,13 @@ import os
 def cdo_daily_means(path, file_includes):
     """
     loops through the given directory and and executes "cdo dayavg *file_includes* file_out"
+    appends "day" at the end of the filename
+    Note: the total file number is currently hard coded to match our 37 years * 12 months lengths of the data
     """
-    for name in os.listdir(path_to_data):
-        if file_includes in name:
-            file_out = f'{path_to_data}{name[:-3]}_day.nc'
-            print(f'cdo dayavg {path_to_data}{name} {file_out}')
-            os.system(f'cdo dayavg {path_to_data}{name} {file_out})
-
+    i = 0
+    for name in os.listdir(path):
+        if file_includes in name and 'day' not in name:
+            i += 1
+            file_out = f'{path}{name[:-3]}_day.nc'
+            print(f'file {i} of {37*12} ...')
+            os.system(f'cdo dayavg {path}{name} {file_out}')
--- python/aux/rename_files.py
+++ python/aux/rename_files.py
@@ -5,13 +5,13 @@ def rename_files(path, old, new, str_constraint=None):
     loops through the given directory and replaces the specified part of a filename with the new part
     the str_constrait variable can be used to only include files, which include a specified string
     """
-    for name in os.listdir(path_to_data):
+    for name in os.listdir(path):
         if str_constraint:
             if str_constraint in name:
                 if old in name:
                     name_new = name.replace(old, new)
-                    os.rename(f'{path_to_data}{name}', f'{path_to_data}{name_new}')
+                    os.rename(f'{path}{name}', f'{path}{name_new}')
         else:
             if old in name:
                 name_new = name.replace(old, new)
-                os.rename(f'{path_to_data}{name}', f'{path_to_data}{name_new}')
+                os.rename(f'{path}{name}', f'{path}{name_new}')
