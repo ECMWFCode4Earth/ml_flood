8e4e687e4127a00b66738a05fbd8d2f5a8ccaa30
Sebastian_Lehner <a1303583@srvx1.img.univie.ac.at>
2019-05-30 03:03:53 +0200

    update docs/004 & preproc

--- python/aux/cdo_daily_means.py
+++ python/aux/cdo_daily_means.py
@@ -3,13 +3,13 @@ import os
 def cdo_daily_means(path, file_includes):
     """
     loops through the given directory and and executes "cdo dayavg *file_includes* file_out"
-    appends "day" at the end of the filename
+    appends "dayavg" at the end of the filename
     Note: the total file number is currently hard coded to match our 37 years * 12 months lengths of the data
     """
     i = 0
     for name in os.listdir(path):
-        if file_includes in name and 'day' not in name:
+        if file_includes in name and 'dayavg' not in name:
             i += 1
-            file_out = f'{path}{name[:-3]}_day.nc'
+            file_out = f'{path}{name[:-3]}_dayavg.nc'
             print(f'file {i} of {37*12} ...')
             os.system(f'cdo dayavg {path}{name} {file_out}')
